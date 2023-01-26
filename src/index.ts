071286e0b1061e81e01167a8dfe8b6ba6873acb0
Matheus Silva <matheussilva.fga@gmail.com>
2023-01-26 16:52:00 -0300

    connecting with mongodb through mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-const port = 3001;
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
-});
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => {
+    console.log('🛑 Erro ao conectar ao mongo');
+  });
+
+
