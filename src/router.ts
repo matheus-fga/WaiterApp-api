b31d1ba4c5f661f5fb7fc664347d122b3e32bf8b
Matheus Silva <matheussilva.fga@gmail.com>
2023-01-27 16:45:55 -0300

    implementing createOrder use case

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,7 @@ import { listProducts } from './app/useCases/products/listProducts';
 import { createProduct } from './app/useCases/products/createProduct';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 import { listOrders } from './app/useCases/orders/listOrders';
+import { createOrder } from './app/useCases/orders/createOrder';
 
 export const router = Router();
 
@@ -41,9 +42,7 @@ router.get('/categories/:categoryId/products', listProductsByCategory);
 router.get('/orders', listOrders);
 
 // Create order
-router.post('/orders', (req, res) => {
-  res.send('OK');
-});
+router.post('/orders', createOrder);
 
 // Change order status
 router.patch('/orders/:orderId', (req, res) => {
