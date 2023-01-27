0280461b7d03530961f96ca9d3aaaa05e1abb973
Matheus Silva <matheussilva.fga@gmail.com>
2023-01-27 15:06:08 -0300

    configuring multer to upload product image

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,24 @@
+import path from 'node:path';
 import { Router } from 'express';
+import multer from 'multer';
 
 import { listCategories } from './app/useCases/categories/listCategories';
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listProducts } from './app/useCases/products/listProducts';
+import { createProduct } from './app/useCases/products/createProduct';
 
 export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    },
+  }),
+});
 
 // List categories
 router.get('/categories', listCategories);
@@ -16,9 +30,7 @@ router.post('/categories', createCategory);
 router.get('/products', listProducts);
 
 // Create product
-router.post('/products', (req, res) => {
-  res.send('OK');
-});
+router.post('/products', upload.single('image'), createProduct);
 
 // Get products by category
 router.get('/categories/:categoryId/products', (req, res) => {
