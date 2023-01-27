1368b4384a3edc9c89382c018415665746057db8
Matheus Silva <matheussilva.fga@gmail.com>
2023-01-27 16:16:14 -0300

    implementing listOrders use case

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,7 @@ import { createCategory } from './app/useCases/categories/createCategory';
 import { listProducts } from './app/useCases/products/listProducts';
 import { createProduct } from './app/useCases/products/createProduct';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { listOrders } from './app/useCases/orders/listOrders';
 
 export const router = Router();
 
@@ -37,9 +38,7 @@ router.post('/products', upload.single('image'), createProduct);
 router.get('/categories/:categoryId/products', listProductsByCategory);
 
 // List orders
-router.get('/orders', (req, res) => {
-  res.send('OK');
-});
+router.get('/orders', listOrders);
 
 // Create order
 router.post('/orders', (req, res) => {
